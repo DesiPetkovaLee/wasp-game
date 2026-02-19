3cef8bfefaf4886ff561bf7c952e5f9bb312c0d6
Desi Petkova Lee <petkovalee@gmail.com>
2026-02-19 14:00:08 +0000

    define wasp trait and classes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,23 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+trait Wasp:
+  val deduction: Int
+  var hitPoints: Int
 
-def msg = "I was compiled by Scala 3. :)"
+  def isDead: Boolean = hitpoints <= 0
+
+  def takeHit(): Unit = 
+  hitPoints = Math.max(0, hitPoints - deduction)
+
+  class Queen extends Wasp:
+    val deduction = 7
+    var hitPoints = 80
+
+  class Worker extends Wasp:
+    val deduction = 10
+    var hitPoints = 68
+
+  class Drone extends Wasp:
+    val deduction = 12
+    var hitPoints = 60
+
+
+
