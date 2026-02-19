5476d28771e5ab864f36f0a24b5ccbdeb75c88f6
Desi Petkova Lee <petkovalee@gmail.com>
2026-02-19 14:46:15 +0000

    Implement Nest factory, status reporting, and Wasp self-identification logic

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,22 +2,34 @@ trait Wasp:
   val deduction: Int
   var hitPoints: Int
 
-  def isDead: Boolean = hitpoints <= 0
+  def isDead: Boolean = hitPoints <= 0
 
   def takeHit(): Unit = 
-  hitPoints = Math.max(0, hitPoints - deduction)
+    hitPoints = Math.max(0, hitPoints - deduction)
 
-  class Queen extends Wasp:
-    val deduction = 7
-    var hitPoints = 80
+  override def toString: String =
+    val name = this.getClass.getSimpleName
+    s"$name: hitPoints = ($hitPoints hit points)"
 
-  class Worker extends Wasp:
-    val deduction = 10
-    var hitPoints = 68
+class Queen extends Wasp:
+  val deduction = 7
+  var hitPoints = 80
 
-  class Drone extends Wasp:
-    val deduction = 12
-    var hitPoints = 60
+class Worker extends Wasp:
+  val deduction = 10
+  var hitPoints = 68
 
+class Drone extends Wasp:
+  val deduction = 12
+  var hitPoints = 60
 
+object Nest:
+  def createWasps(): List[Wasp] = 
+    val queen = List(new Queen())
+    val workers = List.fill(5)(new Worker())
+    val drones = List.fill(8)(new Drone())
+    queen ++ workers ++ drones
 
+  def displayStatus(wasps: List[Wasp]): Unit =
+    println("\nScanning the nest... Here's what's left ->" )
+    wasps.foreach(println)
