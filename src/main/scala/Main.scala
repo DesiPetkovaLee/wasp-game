84fda38658f99bd2328baa819fbf70b682eb11e1
Desi Petkova Lee <petkovalee@gmail.com>
2026-02-20 14:49:10 +0000

    Complete CSV logging, refactor tail recursion, add auto fire extensions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 import scala.util.Random
 import scala.io.StdIn.readLine
+import scala.annotation.tailrec
+import java.io.{FileWriter, PrintWriter}
+import java.io.File
 
 trait Wasp:
   val deduction: Int
@@ -50,13 +53,17 @@ object Game:
 
 @main def startWaspGame(): Unit =
   println("Welcome to the Wasp Game!")
+  println("Commands: 'fire' to shoot, 'auto' for rapid fire, 'restart' to reset, or 'quit' to exit.")
   setupNewGame()
 
 def setupNewGame(): Unit =
+  val name = readLine("\nWho's the wasp slayer today? ")
+  val startTime = System.currentTimeMillis()
   val nestWasps = Nest.createWasps()
-  processCommand(nestWasps)
+  processCommand(nestWasps, name, startTime, 0)
 
-def processCommand(wasps: List[Wasp]): Unit =
+@tailrec
+def processCommand(wasps: List[Wasp], name: String, startTime: Long, shots: Int): Unit =
   val queen = wasps.head
   val input = readLine("Enter your command: ").toLowerCase.trim
 
@@ -64,26 +71,67 @@ def processCommand(wasps: List[Wasp]): Unit =
     case "fire" => 
       if queen.isDead || wasps.forall(w => w.isDead) then
         println("\nThe game is already over! Type 'restart' to play again.")
-        processCommand(wasps)
+        processCommand(wasps, name, startTime, shots)
       else 
         Game.fire(wasps)
         Nest.displayStatus(wasps)
 
+        val newShots = shots + 1
+
         if queen.isDead || wasps.forall(w => w.isDead) then
-          println("\nGAME OVER!")
+          println(s"\nGAME OVER, $name!")
+          println(s"You took $newShots shots.")
+          saveToCSV(name, startTime, newShots)
           println("Type 'restart' to play again or 'quit' to exit.")
-          processCommand(wasps)
+          processCommand(wasps, name, startTime, newShots)
         else
-          processCommand(wasps)
+          processCommand(wasps, name, startTime, newShots)
+
+    case "auto" => 
+      println(s"Auto-mode activated, $name! Taking down wasps at light speed! ")
+      val finalShots = runAutoFire(wasps, shots)
+      Nest.displayStatus(wasps)
+      println(s"\nMISSION ACCOMPLISHED, $name!")
+      saveToCSV(name, startTime, finalShots)
+      println("Type 'restart' to play again or 'quit' to exit.")
+      processCommand(wasps, name, startTime, finalShots)
 
     case "restart" => 
       setupNewGame()
 
     case "quit" =>
-      println("Thanks for playing!")
-
+      println(s"Thanks for playing, $name!")
+      
     case _ => 
-      println("Invalid command. Please enter 'fire', 'restart', or 'quit'.")  
-      processCommand(wasps)
+      println("Invalid command. Please enter 'fire', 'auto', 'restart', or 'quit'.")  
+      processCommand(wasps, name, startTime, shots)
 
-  
+@tailrec
+def runAutoFire(wasps: List[Wasp], count: Int): Int =
+  val queen = wasps.head
+  if queen.isDead || wasps.forall(w => w.isDead) then
+    count
+  else 
+    Game.fire(wasps) 
+    runAutoFire(wasps, count + 1) 
+
+// saving the results of the game in a CSV file
+def saveToCSV(name: String, startTime: Long, finalShots: Int): Unit = 
+  val endTime: Long = System.currentTimeMillis()
+  val duration: Long = (endTime - startTime) / 1000
+
+  val fileName = "results.csv"
+  val file = new File(fileName)
+
+  val isNewFile = !file.exists() || file.length() == 0
+
+  val fileWriter = new FileWriter("results.csv", true)
+  val printWriter = new PrintWriter(fileWriter)
+
+  try 
+    if isNewFile then 
+      printWriter.println("Player Name, Shots Fired, Time (seconds)") 
+      printWriter.println(s"$name,$finalShots,$duration")
+      println(s"\n[Logged to CSV: $name, $finalShots shots, $duration seconds]")
+  finally 
+    printWriter.close()
